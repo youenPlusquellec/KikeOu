41565d0267cb55fe5febf4d82d69ed3975e97298
youen plusquellec <youenetleo@gmail.com>
2021-12-07 21:55:32 +0100

    Add recycler items

--- app/src/main/java/com/example/kikeou/CoworkersFragment.kt
+++ app/src/main/java/com/example/kikeou/CoworkersFragment.kt
@@ -1,12 +1,13 @@
 package com.example.kikeou
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
 import com.example.kikeou.coworkers.CoworkerAdapter
 import com.example.kikeou.databinding.FragmentCoworkersBinding
 import com.example.kikeou.room.models.Agenda
@@ -30,8 +31,7 @@ class CoworkersFragment:Fragment(R.layout.fragment_coworkers) {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentCoworkersBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -39,31 +39,25 @@ class CoworkersFragment:Fragment(R.layout.fragment_coworkers) {
 
         val loc1 = Localisation(1,"18 mai", "dans ton cul")
         val loc2 = Localisation(2,"8 aout", "dans le cul de youen")
-        val contact1 = Contact(1, "tel", "0699328234")
+        val contact1 = Contact(1, "tel", "06.99.32.82.34")
         val contact2 = Contact(2, "email", "wilfried.pepin@outlook")
         val agenda = Agenda(1, "maman" ,50, "une_photo", listOf(contact1, contact2), listOf(loc1, loc2), false)
         val agenda2 = Agenda(2, "papa" ,52, "une_photo", listOf(contact1, contact2), listOf(loc1, loc2), false)
+        val agenda3 = Agenda(3, "moi" ,50, "une_photo", listOf(contact1, contact2), listOf(loc1, loc2), false)
+        val agenda4 = Agenda(4, "toi" ,52, "une_photo", listOf(contact1, contact2), listOf(loc1, loc2), false)
 
-        val agendas = listOf(agenda, agenda2)
+        val agendas = listOf(agenda, agenda2, agenda3, agenda4)
 
         val adapter = CoworkerAdapter()
         binding.coworkersList.adapter = adapter
+        adapter.setOnItemClickListener(object : CoworkerAdapter.onItemClickListener{
+            override fun onItemClick(position: Int) {
+                Toast.makeText(context, "You clicked on n°$position", Toast.LENGTH_SHORT).show()
+            }
 
+        })
         adapter.data = agendas
 
-        //agendas
-
-        /*val adapter = SleepNightAdapter()
-        binding.sleepList.adapter = adapter
-        sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
-            it?.let {
-                adapter.data = it
-            }
-        })*/
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
--- app/src/main/java/com/example/kikeou/coworkers/CoworkerAdapter.kt
+++ app/src/main/java/com/example/kikeou/coworkers/CoworkerAdapter.kt
@@ -2,33 +2,59 @@ package com.example.kikeou.coworkers
 
 import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kikeou.R
 import com.example.kikeou.room.models.Agenda
+import org.w3c.dom.Text
 
-class TextItemViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView)
-
-class CoworkerAdapter : RecyclerView.Adapter<TextItemViewHolder>()  {
+class CoworkerAdapter : RecyclerView.Adapter<CoworkerAdapter.ViewHolder>()  {
 
     var data =  listOf<Agenda>()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
+    private lateinit var mListener: onItemClickListener
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.text_item_view, parent, false) as TextView
-        return TextItemViewHolder(view)
+    interface onItemClickListener {
+
+        fun onItemClick(position: Int)
+
     }
 
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+    fun setOnItemClickListener(listener: onItemClickListener) {
+        mListener = listener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.coworker_item_view, parent, false)
+        return ViewHolder(view, mListener)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.name
+        holder.coworkerName.text = item.name
+        holder.coworkerContact.text = item.contact[0].value
+        //holder.coworkerPicture.text = item.name
     }
 
     override fun getItemCount() = data.size
+
+    class ViewHolder(itemView: View, listener: onItemClickListener) : RecyclerView.ViewHolder(itemView){
+        val coworkerPicture: ImageView = itemView.findViewById(R.id.coworker_picture)
+        val coworkerName: TextView = itemView.findViewById(R.id.coworker_name)
+        val coworkerContact: TextView = itemView.findViewById(R.id.coworker_contact)
+
+        init {
+            itemView.setOnClickListener {
+                listener.onItemClick(adapterPosition)
+            }
+        }
+    }
 
 }
