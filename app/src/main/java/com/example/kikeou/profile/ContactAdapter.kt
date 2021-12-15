f6ba2709cfbdb290d2693aa3f6edc14621c3f4d7
Wilfried PEPIN <wilfried.pepin35133@gmail.com>
2021-12-15 20:19:17 +0100

    contactadapter

--- app/src/main/java/com/example/kikeou/profile/ContactAdapter.kt
+++ app/src/main/java/com/example/kikeou/profile/ContactAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kikeou.R
@@ -14,23 +15,43 @@ import com.example.kikeou.room.models.Agenda
 import com.example.kikeou.room.models.Contact
 import java.util.*
 
-class ContactAdapter : ListAdapter<Agenda, ContactAdapter.MyContactViewHolder>() {
+class ContactAdapter : ListAdapter<Contact, ContactAdapter.MyContactViewHolder>(ContactComparator()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyContactViewHolder {
+        return MyContactViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: MyContactViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.bind(current.value)
+    }
 
 
     class MyContactViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        private val agendaNameZone : EditText = itemView.findViewById(R.id.name_zone)
+        private val contactInfo : TextView = itemView.findViewById(R.id.info)
 
         fun bind(text: String?){
-            agendaNameZone.setText(text)
+            contactInfo.text = text
         }
 
         companion object {
             fun create(parent: ViewGroup): MyContactViewHolder {
                 val view : View = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.activity_add_contact, parent, false)
+                    .inflate(R.layout.profile_item_view, parent, false)
                 return MyContactViewHolder(view)
             }
         }
     }
 
+    class ContactComparator : DiffUtil.ItemCallback<Contact>() {
+        override fun areItemsTheSame(oldItem: Contact, newItem: Contact): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Contact, newItem: Contact): Boolean {
+            return oldItem.value == newItem.value
+        }
+    }
+
 }
+
