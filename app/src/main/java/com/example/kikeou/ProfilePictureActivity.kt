a220f915b95c4e928622255a35586aaf036ec04f
youen plusquellec <youenetleo@gmail.com>
2021-12-19 00:17:31 +0100

    update profile picture list ui

--- app/src/main/java/com/example/kikeou/ProfilePictureActivity.kt
+++ app/src/main/java/com/example/kikeou/ProfilePictureActivity.kt
@@ -26,8 +26,8 @@ class ProfilePictureActivity : AppCompatActivity() {
         "https://i2.wp.com/sguru.org/wp-content/uploads/2017/06/cool-anonymous-profile-pictures-15.jpg?resize=460%2C458&ssl=1",
         "https://www.postplanner.com/hs-fs/hub/513577/file-2886416984-png/blog-files/facebook-profile-pic-vs-cover-photo-sq.png?width=250&height=250&name=facebook-profile-pic-vs-cover-photo-sq.png",
         "https://static.independent.co.uk/s3fs-public/thumbnails/image/2015/06/24/19/gay-pride.jpg?width=982&height=726&auto=webp&quality=75",
-        "https://lh3.googleusercontent.com/proxy/9io-JZk38MscM2OrFWUGTdvv1Lt-R-piY41U497dDBHFR0cDb3q_BF32p3eC3RAIdP-AJqkSTU4sLTcCtVMZRqL3jsnwiK_20m8",
-        "https://cdn.radiofrance.fr/s3/cruiser-production/2019/03/b80afc41-0af5-4496-8a68-3b753b1d0474/1136_gettyimages-151324506.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOVz5_CJ8OFSCDGhOlFOpyyDmAhHHTpXy0q37WIiuUyUvs_6PQXaTgvGMZVfz70yNVqH8&usqp=CAU",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/260px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg",
         "https://p16-va-default.akamaized.net/img/musically-maliva-obj/1655574325839877~c5_1080x1080.jpeg"
     )
 
