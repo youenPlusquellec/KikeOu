2968184dffad997c7b6acf1c7515506f20324996
Kevin <kplestan@enssat.fr>
2021-12-26 05:32:49 +0100

    :monocle_face: Cleaning debug log...

--- app/src/main/java/fr/enssat/kikeou/pepin_plestan_plusquellec/fragment/CoworkersFragment.kt
+++ app/src/main/java/fr/enssat/kikeou/pepin_plestan_plusquellec/fragment/CoworkersFragment.kt
@@ -1,18 +1,12 @@
 package fr.enssat.kikeou.pepin_plestan_plusquellec.fragment
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import fr.enssat.kikeou.pepin_plestan_plusquellec.AppApplication
@@ -37,16 +31,12 @@ class CoworkersFragment:Fragment(R.layout.fragment_coworkers) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        Log.d("MERDOUILLE", "OnCreateView")
-
         binding = FragmentCoworkersBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        Log.d("MERDOUILLE", "ViewCreated")
 
         val adapter = CoworkerAdapter()
         binding.coworkersList.adapter = adapter
--- app/src/main/java/fr/enssat/kikeou/pepin_plestan_plusquellec/viewmodel/CoworkerViewModel.kt
+++ app/src/main/java/fr/enssat/kikeou/pepin_plestan_plusquellec/viewmodel/CoworkerViewModel.kt
@@ -10,55 +10,33 @@ import kotlinx.coroutines.launch
 class CoworkerViewModel(private val repository: AgendaRepository) : ViewModel() {
     val agendas: LiveData<List<Agenda>> = repository.others.asLiveData()
 
-    fun insertOrUpdate(agenda: Agenda)
-    {
-        Log.d("SA MERE", "YO!")
-        val test = viewModelScope.launch {
-            val dbData = repository.findByName(agenda.name)
+    fun insertOrUpdate(agenda: Agenda) = viewModelScope.launch {
+        val dbData = repository.findByName(agenda.name)
 
-            when {
-                dbData.isEmpty() -> {
-                    Log.w("KIKEOU", "DB - Empty")
-                    agenda.id = 0
-                    repository.insert(agenda)
-                }
-                dbData[0].is_mine -> {
-                    Log.w("KIKEOU", "DB - No empty / is_mine")
-                    agenda.id = 0
-                    repository.insert(agenda)
-                }
-                else -> {
-                    Log.w("KIKEOU", "DB - Else")
-                    agenda.id = dbData[0].id
-                    repository.update(agenda)
-                }
+        when {
+            dbData.isEmpty() -> {
+                agenda.id = 0
+                repository.insert(agenda)
             }
-        }
-
-        test.invokeOnCompletion { cause ->
-            if(cause == null)
-                Log.d("NIQUE", "NULL sa mère")
-            else if(cause is CancellationException)
-                Log.d("NIQUE", cause.message ?: "")
-            else
-                Log.d("NIQUE", "BRANLE")
+            dbData[0].is_mine -> {
+                agenda.id = 0
+                repository.insert(agenda)
+            }
+            else -> {
+                agenda.id = dbData[0].id
+                repository.update(agenda)
+            }
         }
     }
 
     fun delete(agenda: Agenda) = viewModelScope.launch {
         repository.delete(agenda)
     }
-
-    override fun onCleared() {
-        super.onCleared()
-        Log.d("NIQUE", "On tire la chasse...")
-    }
 }
 
 class CoworkerViewModelFactory(private val repository: AgendaRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(CoworkerViewModel::class.java)) {
-            Log.d("TROLOLO", "Création ?")
             @Suppress("UNCHECKED_CAST")
             return CoworkerViewModel(repository) as T
         }
